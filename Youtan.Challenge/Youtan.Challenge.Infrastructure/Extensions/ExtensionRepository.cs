643bc329fe339a7047b303f724e4d58b89e1beab
Natan Roberto Xavier <121309547+natanrobertoxavier@users.noreply.github.com>
2025-04-25 14:21:34 -0300

    fix: removed logs

--- Youtan.Challenge/Youtan.Challenge.Infrastructure/Extensions/ExtensionRepository.cs
+++ Youtan.Challenge/Youtan.Challenge.Infrastructure/Extensions/ExtensionRepository.cs
@@ -19,8 +19,7 @@ public static class ExtensionRepository
     {
         var databaseName = configurationManager.GetDatabaseName();
         var connection = configurationManager.GetConnection();
-        Console.WriteLine("#################################################################################################################");
-        Console.WriteLine($"{connection}Database={databaseName};");
+
         return $"{connection}Database={databaseName};";
     }
 }
